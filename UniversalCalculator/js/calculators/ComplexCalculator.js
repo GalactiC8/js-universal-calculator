bf17825e4bd2cb2ae86cfd1ac4e35e19ce7f8f51
Kirill Troshkov <futuregalactic@proton.me>
2023-12-04 14:04:11 +0400

    Real and Complex Calcs
    +красивое оформление

--- UniversalCalculator/js/calculators/ComplexCalculator.js
+++ UniversalCalculator/js/calculators/ComplexCalculator.js
@@ -1,22 +1,36 @@
 class ComplexCalculator extends RealCalculator {
     add(a, b) {
-        return new Complex(a.re + b.re, a.im + b.im)
+        return new Complex(a.re + b.re, a.im + b.im);
     }
     sub(a, b) {
-        return new Complex(a.re - b.re, a.im - b.im)
+        return new Complex(a.re - b.re, a.im - b.im);
     }
     mult(a, b) {
-        return new Complex(a.re * b.re, a.im * b.im)
+        return new Complex(
+            a.re * b.re - a.im * b.im,
+            a.re * b.im + a.im * b.re
+        );
+    }
+    prod(a, p) {
+        return new Complex(a.re * p.re, a.im * p.re);
     }
     div(a, b) {
-        return new Complex(a.re / b.re, a.im / b.im)
+        const m = (b.re ** 2 + b.im ** 2);
+        return new Complex(
+            (a.re * b.re + a.im * b.im) / m,
+            (a.im * b.re - a.re * b.im) / m
+        );
     }
-    pow(a, b) {
-        return new Complex(Math.exp(a.re) * Math.cos(b.im),
-                           Math.exp(a.re) * Math.sin(b.im));
+    pow(a, n) {
+        let S = this.one();
+        for (let i = 0; i < n.re; i++) {
+            S = this.mult(S, a);
+            console.log(S);
+        }
+        return S;
     }
     one() {
-        return new Complex (super.one());
+        return new Complex(super.one());
     }
     zero() {
         return new Complex;
--- UniversalCalculator/js/main.js
+++ UniversalCalculator/js/main.js
@@ -1,16 +1,32 @@
 window.onload = function () {
-    const inputA = document.querySelector('.input_a');
+    const inputA = document.getElementById('input_a');
     const selectOperation = document.querySelector('.select_operation');
-    const inputB = document.querySelector('.input_b');
+    const inputB = document.getElementById('input_b');
     const buttonResult = document.querySelector('.buttonResult');
     const output = document.querySelector('.output');
+
+    const inputA_RE = document.getElementById('input_a_re');
+    const inputA_IM = document.getElementById('input_a_im');
+    const selectOperationComplex = document.querySelector('.select_operation_complex');
+    const inputB_RE = document.getElementById('input_b_re');
+    const inputB_IM = document.getElementById('input_b_im');
+    const buttonResultComplex = document.querySelector('.buttonResultComplex');
+    const outputComplex = document.querySelector('.outputComplex');
 
     buttonResult.addEventListener('click', function() {
         const a = inputA.value - 0;
         const b = inputB.value - 0;
         const operation = selectOperation.value;
-        const result = calculate({ a, b, operation });
-
-        output.innerHTML = result;
+        const realCalc = new RealCalculator;
+        output.innerHTML = realCalc[operation](a, b);
     });
-}
+
+    buttonResultComplex.addEventListener('click', function() {
+        const a = new Complex(inputA_RE.value, inputA_IM.value);
+        const b = new Complex(inputB_RE.value, inputB_IM.value);
+        const operation = selectOperationComplex.value;
+        const complexCalc = new ComplexCalculator;
+        const {re, im} = complexCalc[operation](a, b);
+        outputComplex.innerHTML = `Re: ${re} Im: ${im}`
+    });
+} 
